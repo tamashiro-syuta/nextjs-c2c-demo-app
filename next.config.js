80e79d1d7325717b316eb186952a241b27520021
tamashiro-syuta <syuta5825@gmail.com>
2022-10-08 16:41:32 +0900

    styled-componentsの有効化

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  compiler: {
+    // styled-componentsの有効化
+    styledComponents: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
