fb8f143bcf62b83241979fdf086b720cf8b32762
tamashiro-syuta <syuta5825@gmail.com>
2022-10-08 17:10:02 +0900

    createGlobalStylesを使って、定義したスタイルをグローバルに適用

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,50 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { createGlobalStyle } from "styled-components";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const GlobalStyles = createGlobalStyle`
+  html,
+  body,
+  textarea {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system,Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fria Sans, Driod Sans, Helvetica Neue, sans-serif;
+  }
 
-export default MyApp
+  * {
+    box-sizing: border-box;
+  }
+
+  a {
+    corsor: pointer;
+    text-decoration: none;
+    transition: .25s;
+    color: #000;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+`;
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <meta key="charset" name="charset" content="utf-8" />
+        <meta
+          key="viewport"
+          name="viewport"
+          content="width = device-width, initial-scale = 1, shrink-to-fit = no, maximum-scale = 5"
+        />
+        <meta property="og:locale" content="ja_JP" />
+        <meta property="og:type" content="website" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default MyApp;
