b15eea8968fa0314c0894444ab488e1cae8adb38
tamashiro-syuta <syuta5825@gmail.com>
2022-10-09 12:33:37 +0900

    アドオンを追加(WebpackFinalの設定は、必要なアドオンを導入し、tsconfigの設定を引き継ぐためのもの)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const path = require("path");
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -5,9 +8,23 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-webpack5",
-  },
   staticDirs: ["public"],
+  babel: async (options) => ({
+    ...options,
+    plugins: [
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-proposal-private-methods",
+      "@babel/plugin-proposal-private-property-in-object",
+    ],
+  }),
+  // WebpackFinalの設定は、必要なアドオンを導入し、tsconfigの設定を引き継ぐためのもの
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, "../tsconfig.json"),
+      }),
+    ];
+
+    return config;
+  },
 };
