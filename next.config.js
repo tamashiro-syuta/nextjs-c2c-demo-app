d06304969038d61df028a6a5167a096351c436b9
tamashiro-syuta <syuta5825@gmail.com>
2022-10-09 13:27:31 +0900

    テスト環境の構築

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    // styled-componentsの有効化
-    styledComponents: true,
-  },
+  compiler: (() => {
+    let compilerConfig = {
+      // styled-componentsの有効化
+      styledComponents: true,
+    };
+
+    if (process.env.NODE_ENV === "production") {
+      compilerConfig = {
+        ...compilerConfig,
+        // 本番環境では、ReactTestingLibraryで使用するdata-testid属性を削除
+        reactRemoveProperties: { properties: ["^data-testid$"] },
+      };
+    }
+
+    return compilerConfig;
+  })(),
 };
 
 module.exports = nextConfig;
