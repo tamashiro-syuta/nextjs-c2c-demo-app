e70d60b64d7205c2ddd0b4c9c7a724f77576ae98
tamashiro-syuta <syuta5825@gmail.com>
2022-10-10 16:35:22 +0900

    CORS用にRewrite機能を設定

--- next.config.js
+++ next.config.js
@@ -17,6 +17,19 @@ const nextConfig = {
 
     return compilerConfig
   })(),
+  // オリジン間リソース共有(CORS)をでのCookie送信を避けるために、nextのRewrites機能を使用して、プロキシを設定 (ReWrites機能は、指定したURLパターンを内部で別のURLに変換する機能)
+  // nextのエンドポイントにリクエストを送信するとjson-serverのエンドポイントに変換されてリクエストを送信するようにする
+  // ex. http://nextjs/api/proxy/siginin → http://signin と変換する
+  async rewrites() {
+    return [
+      {
+        // ex. /api/proxy
+        source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:match*`,
+        // ex. http://localhost:8000
+        destination: `${process.env.API_BASE_URL}/:match*`,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
